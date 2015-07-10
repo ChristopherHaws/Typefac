63696cf6c45e1e7a6d2dafde8aa56adb0086658a
Christopher Haws <cribs2@gmail.com>
2015-07-10 08:34:10 -0700

    Added missing gulpfile's

---
+++ Typefac.Tests/gulpfile.js
@@ -0,0 +1,22 @@
+﻿/// <binding AfterBuild='default' />
+var gulp = require("gulp");
+var tsc = require("gulp-tsc");
+
+gulp.task("default", function () {
+
+	gulp
+		.src("../artifacts/bin/Typefac/*.*")
+		.pipe(gulp.dest("./wwwroot/libs/"));
+
+	gulp
+		.src(["./Tests/**/*.ts", "./libs/**/*.d.ts", "./wwwroot/libs/Typefac.d.ts"])
+		.pipe(tsc({
+			"module": "amd",
+			"noImplicitAny": false,
+			"noEmitOnError": true,
+			"removeComments": false,
+			"sourceMap": true,
+			"target": "es5",
+			"out": "../wwwroot/Tests.js"
+		}));
+});
---
+++ Typefac/gulpfile.js
@@ -0,0 +1,18 @@
+﻿/// <binding BeforeBuild='build' />
+var gulp = require("gulp");
+var tsc = require("gulp-tsc");
+
+gulp.task("build", function () {
+	gulp
+		.src(["./**/*ts", "!./{node_modules,node_modules/**}", "!./{bower_components,bower_components/**}"])
+		.pipe(tsc({
+			"module": "amd",
+			"noImplicitAny": false,
+			"noEmitOnError": true,
+			"removeComments": false,
+			"sourceMap": true,
+			"target": "es5",
+			"out": "../../artifacts/bin/Typefac/Typefac.js",
+			"declaration": true
+		}));
+});
